3ed15076bfb250ba0de6197e937c9cf42170891d
MD-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-04-30 00:33:06 +0600

    api route added

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+require('dotenv/config');
+
+
+//Middleware
+app.use('/posts', ()=>{
+    console.log('you are in posts middleware coosole');
+});
+
+
+//Import Routes
+const postsRoutes = require('./routes/posts');
+app.use('/posts',postsRoutes);
+
+//ROUTES
+app.get('/',(req,res)=>{
+    res.send('we are on home');
+});
+
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,
+
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('connected to server'),
+);
+
+
+
+app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/posts',(req,res)=>{
+    res.send('we are on posts');
+});
+
+module.exports = router;
